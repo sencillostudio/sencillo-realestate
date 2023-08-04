4433b57bdd6ec3606265bedb0af0b929a4f9c013
miltonzini <miltonzini@gmail.com>
2023-08-04 03:55:25 -0300

    migrate navbar to module

---
+++ includes/modules/navbar.php
@@ -0,0 +1,22 @@
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">Sencillo Real Estate</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="properties.php">Properties</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#about">About us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#contact">Leave us a message</a>
+            </li>
+            
+            </ul>
+        </div>
+        </div>
+    </nav>
--- index.php
+++ index.php
@@ -9,28 +9,7 @@
   </head>
 
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a class="navbar-brand" href="#">Sencillo Real Estate</a>
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="properties.php">Properties</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#about">About us</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#contact">Leave us a message</a>
-            </li>
-          
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <?php include_once 'includes/modules/navbar.php' ?>
 
     <div class="heroSection p-5">
       <div class="container py-5">
