5283b673175670a058c4b92287f7c6783f4fe1f2
miltonzini <miltonzini@gmail.com>
2023-08-06 16:45:59 -0300

    Update subHeroSection.php

--- includes/modules/subHeroSection.php
+++ includes/modules/subHeroSection.php
@@ -1,5 +1,5 @@
 <div class="subHeroSection p-5">
-    <div class="container py-5">
-    <h1 class="display-5 fw-bold">Properties</h1>
+    <div class="container py-2">
+    <h1 class="display-5 fw-bold"><?php echo $pageTitle ?></h1>
     </div>
 </div>
