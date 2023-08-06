e2d1121442f7e499827fdf5c1b32941317c1885d
miltonzini <miltonzini@gmail.com>
2023-08-06 16:42:50 -0300

    migrte gallery to module

---
+++ includes/modules/gallerySection.php
@@ -0,0 +1,5 @@
+<section class="gallery">
+    <div class="container">
+    <p>Gallery section. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, explicabo voluptate? Tempore incidunt deleniti delectus similique ad voluptate, optio rerum necessitatibus laborum? Quisquam, optio odit! Perspiciatis saepe repellendus corporis recusandae.</p>
+    </div>
+</section>
--- property.php
+++ property.php
@@ -13,11 +13,7 @@
 
     <?php include_once 'includes/modules/subHeroSection.php' ?>
 
-    <section class="gallery">
-      <div class="container">
-        <p>Gallery section. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, explicabo voluptate? Tempore incidunt deleniti delectus similique ad voluptate, optio rerum necessitatibus laborum? Quisquam, optio odit! Perspiciatis saepe repellendus corporis recusandae.</p>
-      </div>
-    </section>
+    <?php include_once 'includes/modules/gallerySection.php' ?>
 
     <?php include_once 'includes/modules/statementSection.php'?>
 
