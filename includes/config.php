27f2c9a66da3e394587ab145a415c246afce005c
miltonzini <miltonzini@gmail.com>
2023-08-06 21:30:10 -0300

    update gitignore
    require publicConfig.php

---
+++ includes/config.php
@@ -0,0 +1,22 @@
+<?php
+    require_once('env.php'); // envirorment
+    require_once('functions.php');
+    require_once('publicConfig.php'); // user config
+
+    // DB Connections
+    if ($env === 'local') {
+        require_once('db-local.php');
+    } else {
+        require_once('db-production.php');
+    }
+    
+
+    // Base Url
+    if ($env === 'local') {
+        define('BASE_URL', '...');
+    } else {
+        define('BASE_URL', '...'); 
+    }
+
+    // note: real BASE_URL will remain hidden in this public repo. Real info will exist only in dev (local) and in server.
+?>
