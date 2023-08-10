0830bcabd524bf99333df58a4897c08dda54ae90
miltonzini <miltonzini@gmail.com>
2023-08-10 01:05:43 -0300

    update description section

--- includes/modules/descriptionSection.php
+++ includes/modules/descriptionSection.php
@@ -1,9 +1,10 @@
-<section class="reg-section reg-section-soft-gradient" id="descriptionSection">
+<section class="reg-section reg-section-color-gradient" id="descriptionSection">
     <div class="container">
         <h2 class="">Description</h2>
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore similique maiores eveniet perspiciatis, nulla quae sit, itaque unde a assumenda excepturi, labore nemo dolorum. Sequi pariatur aliquam laboriosam labore officiis. <br>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, molestias ratione? Quas, eligendi. Molestiae laudantium est necessitatibus libero assumenda at explicabo ex in facere fugiat laborum ut ducimus, praesentium dolore?
+        <p class="col-md-8 fs-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nulla, facere consequatur amet voluptates a eos, quos quae odio quidem aperiam excepturi! Delectus consequatur quasi ad saepe harum similique accusantium!</p><p class="col-md-8 fs-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nisi earum iure quibusdam quod dolor hic. Incidunt temporibus, labore dolore voluptatibus unde explicabo magni, amet, minus esse voluptates sit commodi?
+            <br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui aliquid molestias accusamus hic amet possimus quasi sed sunt? In illum cumque est eveniet molestias magnam tempore quas illo itaque vero!
         </p>
     </div>
 </section>
