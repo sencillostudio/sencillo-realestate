77fed6b4e4f3cdccf3a64acb142483dc49d47adb
miltonzini <miltonzini@gmail.com>
2023-09-01 02:35:41 -0300

    Update app.php
    update BASE_DIR for production

--- includes/app.php
+++ includes/app.php
@@ -16,13 +16,13 @@
     
     
 
-    // Base Url
+    // Base Url / Directory
     if ($env === 'local') {
         define('BASE_URL', 'http://localhost/sencillo-realestate');
         define('BASE_DIR', 'D:/programfilesmz/xampp/htdocs/sencillo-realestate');
     } else {
         define('BASE_URL', 'https://mzdev.com.ar/projects/sencillo-realestate');
-        define('BASE_DIR', '/projects/sencillo-realestate');
+        define('BASE_DIR', '/home/u728248526/domains/mzdev.com.ar/public_html/projects/sencillo-realestate');
     }
     // note: real BASE_URL will remain hidden in this public repo. Real info will exist only in dev (local) and in server.
 
