9143dad1d0c3da16f2129d8064f6ff92149ab217
miltonzini <miltonzini@gmail.com>
2023-09-20 02:12:02 -0300

    Update adminCardsSection.php
    update loop and values

--- includes/modules/adminCardsSection.php
+++ includes/modules/adminCardsSection.php
@@ -1,15 +1,15 @@
 <section class="cardsSection">
     <div class="container">
         <div class="row row-cols-xs-1 row-cols-sm-2 row-cols-lg-4 g-3">
-            <?php for ($i=0; $i < 7; $i++) : ?>
+            <?php foreach($properties as $property): ?>
             <div class="col">
                 <div href="property.php" class="card" >
                     <div class="card-cover">
-                        <img src="../src/img/misc/property-cover-example.png" class="card-img-top" alt="...">
+                        <img src="<?php echo $property->img1 ?>" class="card-img-top" alt="...">
                     </div>
                     <div class="card-content">
-                        <h5 class="card-title">Property Name</h5>
-                        <p class="code"><strong>code:</strong> prop0053</p>
+                        <h5 class="card-title"><?php echo $property->name ?></h5>
+                        <p class="code"><strong>code:</strong> <?php echo $property->code ?></p>
                         <div class="btn-wrapper mt-3 d-flex flex-column">
                             <a href="#" class="btn btn-sm btn-primary mt-1">Edit property</a>
                             <a href="#" class="btn btn-sm btn-danger mt-1">Delete property</a>
@@ -17,7 +17,7 @@
                     </div>
                 </div>
             </div>
-            <?php endfor; ?>
+            <?php endforeach; ?>
         </div>
     </div>
 </section>
